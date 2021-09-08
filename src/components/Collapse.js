915b7390c6ee7a966ac4db0f467e98df50cf9056
Furkan Zerman <59289764+Swindler36@users.noreply.github.com>
2021-09-08 13:59:58 +0300

    components was moved to basic-components-kapseli
    Repo Url: https://github.com/Stingy-Developer/basic-components-kapseli

--- src/components/Collapse.js
+++
@@ -1,9 +0,0 @@
-import { Component } from "./Component";
-
-export class Collapse extends Component{
-    constructor(config){
-        this.title = config.title;
-        this.items = config.items;
-    }
-
-}
