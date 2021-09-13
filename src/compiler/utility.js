5d46deb19d9eace4180295e9ddc0e0c63fa1ab3e
Furkan Zerman <59289764+Swindler36@users.noreply.github.com>
2021-09-13 13:33:23 +0300

    prettier format

--- src/compiler/utility.js
+++ src/compiler/utility.js
@@ -1,27 +1,24 @@
 const className = (classList_obj = {}) => {
-    let enable_classes = [];
-    for (const klass in classList_obj) {
-        if (Object.hasOwnProperty.call(classList_obj, klass)) {
-            const is_enable = classList_obj[klass];
-            if(is_enable){
-                enable_classes.push(klass);
-            }
-        }
+  let enable_classes = [];
+  for (const klass in classList_obj) {
+    if (Object.hasOwnProperty.call(classList_obj, klass)) {
+      const is_enable = classList_obj[klass];
+      if (is_enable) {
+        enable_classes.push(klass);
+      }
     }
-    return enable_classes.join(" ");
-}
+  }
+  return enable_classes.join(" ");
+};
 
 const style = (style_obj = {}) => {
-    let styles = [];
-    for (const key in style_obj) {
-        if (Object.hasOwnProperty.call(style_obj, key)) {
-            styles.push(key+":"+style_obj[key]);
-        }
+  let styles = [];
+  for (const key in style_obj) {
+    if (Object.hasOwnProperty.call(style_obj, key)) {
+      styles.push(key + ":" + style_obj[key]);
     }
-    return styles.join(";");
-}
+  }
+  return styles.join(";");
+};
 
-export {
-    className,
-    style
-}
+export { className, style };
