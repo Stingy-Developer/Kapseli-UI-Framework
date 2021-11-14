d856472d6e4b9cc85f0a9c012f9e04a0a2010be4
Furkan Zerman <59289764+Swindler36@users.noreply.github.com>
2021-11-14 19:53:16 +0300

    Formatting

--- src/index.js
+++ src/index.js
@@ -1,28 +1,24 @@
-window.jsx = (tag,props,...children) => {
-    if(tag == 'fragment') return children;
+window.jsx = (tag, props, ...children) => {
+  if (tag == "fragment") return children;
+  for (const prop in props) {
+    if (Object.hasOwnProperty.call(props, prop)) {
+      const prop_value = props[prop];
 
-    for (const prop in props) {
-        if (Object.hasOwnProperty.call(props, prop)) {
-            const prop_value = props[prop];
-
-            if(prop.startsWith("on")){
-                props[ prop.replace("on","@") ] = prop_value;
-                delete props[prop];
-            }
-            
-        }
+      if (prop.startsWith("on")) {
+        props[prop.replace("on", "@")] = prop_value;
+        delete props[prop];
+      }
     }
-    return {
-        tag: tag.toUpperCase(),
-        props: props ? props : {},
-        children
-    }
-}
-
+  }
+  return {
+    tag: tag.toUpperCase(),
+    props: props ? props : {},
+    children,
+  };
+};
 
 import { Kapseli } from "./app/app";
 import { Component } from "./components/Component";
-
 
 window.Kapseli = Kapseli;
 Kapseli.Component = Component;
