c174345afcd0a16dcad217d8c6f77b31b19bf4a8
Furkan Zerman <59289764+Swindler36@users.noreply.github.com>
2021-12-01 01:51:01 +0300

    Refactoring for setting provider

--- src/app/configs/provider.js
+++ src/app/configs/provider.js
@@ -1,8 +1,6 @@
 import { FloatPanel, StaticPanel } from "../../components/Panel";
 import { Provider } from "../../components/Provider";
 export function setProvider(self) {
-  self.View.addComponent("PROVIDER", new Provider());
-  self.View.addComponent("left-pan", new FloatPanel("start", "left-pan"));
-  self.View.addComponent("right-pan", new FloatPanel("end", "right-pan"));
-  self.View.addComponent("bot-pan", new FloatPanel("bottom", "bot-pan"));
+  self.View.addComponent("PROVIDER", Provider);
+  self.View.addComponent("PANEL", FloatPanel);
 }
