4fa3cc6d46fcd770fd846d82f8f011890b113498
Furkan Zerman <59289764+Swindler36@users.noreply.github.com>
2022-01-05 14:51:23 +0300

    refactor floatpanels

--- src/app/configs/command.js
+++ src/app/configs/command.js
@@ -1,34 +1,38 @@
-export function setCommands(self){
-    self.Command.add("storage:load",async function(cb){
-        await self.Storage.load();
-        cb();
+export function setCommands(self) {
+  self.Command.add("storage:load", async function (cb) {
+    await self.Storage.load();
+    cb();
+  });
+
+  self.Command.add("storage:store", async function (cb) {
+    await self.Storage.store();
+    cb();
+  });
+
+  let static_panels = ["left-side", "right-side", "nav", "status"];
+  let float_panels = ["left-pan", "right-pan", "bot-pan"];
+
+  static_panels.forEach((p) => {
+    self.Command.add(`panel:${p}:show`, function () {
+      self.Panel.show(p);
     });
 
-    self.Command.add("storage:store",async function(cb){
-        await self.Storage.store();
-        cb();
+    self.Command.add(`panel:${p}:hide`, function () {
+      self.Panel.hide(p);
+    });
+  });
+
+  float_panels.forEach((p) => {
+    self.Command.add(`panel:${p}:open`, function () {
+      self.View._component_memo[p].$options.show();
     });
 
-    let static_panels = ["left-side","right-side","nav","status"];
-    let float_panels = ["left-pan","right-pan","bot-pan"];
-
-    static_panels.forEach(p => {
-        self.Command.add(`panel:${p}:show`,function(){
-            self.Panel.show(p);
-        });
-
-        self.Command.add(`panel:${p}:hide`,function(){
-            self.Panel.hide(p);
-        });
+    self.Command.add(`panel:${p}:close`, function () {
+      self.View._component_memo[p].$options.hide();
     });
 
-    float_panels.forEach(p => {
-        self.Command.add(`panel:${p}:open`,function(){
-            self.Panel.show(p);
-        });
-
-        self.Command.add(`panel:${p}:close`,function(){
-            self.Panel.hide(p);
-        });
+    self.Command.add(`panel:${p}:toggle`, function () {
+      self.View._component_memo[p].$options.toggle();
     });
+  });
 }
