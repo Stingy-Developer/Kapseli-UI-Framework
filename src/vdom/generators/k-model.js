094de04571bec2f8e8f5ae99419c7b8578073667
Furkan Zerman <59289764+Swindler36@users.noreply.github.com>
2022-01-05 14:54:08 +0300

    set KModel

---
+++ src/vdom/generators/k-model.js
@@ -0,0 +1,36 @@
+export function setKModel(obj) {
+  obj.addGenerator("k-model", function (expression, vdom, self) {
+    let data = self.getData(expression);
+
+    switch (vdom.props["type"]) {
+      case "checkbox":
+        vdom.props["@input"] = "k_model_input_checked";
+        vdom.props["checked"] = data;
+        break;
+
+      case "file":
+      case "image":
+        vdom.props["@input"] = "k_model_input_files";
+        break;
+
+      default:
+        vdom.props["@input"] = "k_model_input_value";
+        vdom.props["value"] = data;
+        break;
+    }
+
+    vdom.component_uuid = "__GLOBAL__";
+
+    self.methods["__GLOBAL__"] = {
+      k_model_input_value(e) {
+        self.data[expression] = e.target.value;
+      },
+      k_model_input_checked(e) {
+        self.data[expression] = e.target.checked;
+      },
+      k_model_input_files(e) {
+        self.data[expression] = e.target.files;
+      },
+    };
+  });
+}
