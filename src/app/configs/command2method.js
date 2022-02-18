fc5c39d0056f32d4b2f8870a2ec9e97a8504feea
Furkan Zerman <59289764+Swindler36@users.noreply.github.com>
2022-02-18 14:05:11 +0300

    add `command2method`

---
+++ src/app/configs/command2method.js
@@ -0,0 +1,14 @@
+export const command2method = (Command) => {
+  let COMMANDS = {};
+
+  let all_commands = Command.getAll();
+  for (const command in all_commands) {
+    if (Object.hasOwnProperty.call(all_commands, command)) {
+      COMMANDS["cmd:" + command] = all_commands[command].run;
+      COMMANDS["cmd:" + command + ":run"] = all_commands[command].run;
+      COMMANDS["cmd:" + command + ":stop"] = all_commands[command].stop;
+    }
+  }
+
+  return COMMANDS;
+};
